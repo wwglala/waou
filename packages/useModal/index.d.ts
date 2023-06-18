58ee1f9b2ed0c9528220e1a42869ce632f7b1a2d
wangweiguang <wangweiguang.1016@bytedance.com>
2023-06-18 22:59:51 +0800

    version: 2.0.0

--- packages/useModal/index.d.ts
+++ packages/useModal/index.d.ts
@@ -10,18 +10,30 @@ interface ModalProviderProps {
     children: ReactNode;
 }
 interface Param2Props<T> {
-    modalProps: T;
+    modalProps?: T & {
+        __freeze?: boolean;
+    };
     [x: string]: unknown;
 }
 interface Dispatch<T> {
-    (visible: boolean, props: Param2Props<T>): void;
+    (visible: boolean, props?: Param2Props<T>): Promise<{
+        value: any;
+        onClose: () => void;
+    }>;
 }
 interface useModalHandler {
-    <T>(FunctionComponent: FunctionComponent, props: Param2Props<T>, deps: Readonly<unknown[]>): [Dispatch<T>];
+    <T>(FunctionComponent: FunctionComponent<any>, props?: Param2Props<T>, deps?: Readonly<unknown[]>): [Dispatch<T>];
 }
 
 declare function ModalProvider(props: ModalProviderProps): JSX.Element;
 
 declare const useModal: useModalHandler;
 
-export { ModalProvider, useModal };
+declare const useInjectProps: <M>() => {
+    injectModalProps: (props: M) => void;
+    onClose: () => void;
+    onResolve: (value: unknown) => void;
+    onReject: (err: any) => void;
+};
+
+export { ModalProvider, useInjectProps, useModal };
