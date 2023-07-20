bf0eb0caaa92322ab844cb5460a7f6f6dd27bae2
wangweiguang <wangweiguang.1016@bytedance.com>
2023-07-20 11:15:44 +0800

    chore: lint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 0,
+    'react/display-name': 0,
+    'react-hooks/exhaustive-deps': 0,
+  },
+};
